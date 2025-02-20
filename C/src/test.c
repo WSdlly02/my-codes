0332a9ac156239a34941c13b49d328b5e0633b10
wsdlly02 <wsdlly02@hotmail.com>
2025-02-20 20:55:07 +0800

    ...

--- C/src/test.c
+++ C/src/test.c
@@ -3,15 +3,7 @@
 int
 main ()
 {
-  int i, j;
-  for (i = 0; i < 8; i++)
-    {
-      for (j = 0; j < 8; j++)
-        if ((i + j) % 2 == 0)
-          printf ("%c%c", 219, 219);
-        else
-          printf ("  ");
-      printf ("\n");
-    }
-  return 0;
+  int i=1, j;
+	j = (++i) + (++i);
+	printf("%d\n",j);
 }
