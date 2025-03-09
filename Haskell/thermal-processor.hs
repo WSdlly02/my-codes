fc1b0541e56cf5a8bc898e807ae5ab16e68b6610
wsdlly02 <wsdlly02@hotmail.com>
2025-03-09 20:28:21 +0800

    thermal data resolver

--- Haskell/thermal-processor.hs
+++ Haskell/thermal-processor.hs
@@ -19,16 +19,27 @@ main :: IO ()
 main = do
     hSetBuffering stdin NoBuffering
     let chunkSize = 8 + 4 * 768
-    
+
     forever $ do  -- 现在 forever 已可用
         bytes <- B.hGet stdin chunkSize
         case runGetOrFail parseThermalData (BL.fromStrict bytes) of
-            Left (_, _, err) -> 
+            Left (_, _, err) ->
                 putStrLn $ "解析错误: " ++ err
             Right (_, _, (ts, temps)) -> do
-                let maxTemp = maximum temps
-                -- let warnTempArray = filter (>30) temps
+                let maxTemp = maximum temps -- 求最大温度
+                let tempArray = array2dim temps where -- 将768个温度数据存进32*24的二维矩阵
+                    array2dim [] = []
+                    array2dim (xs) = take 32 (xs) : array2dim (drop 32 xs)
+                let warnTempArray = refill2DimArrayWithTemp tempArray where -- 过滤矩阵，记录危险温度
+                    refill2DimArrayWithTemp [] = []
+                    refill2DimArrayWithTemp (x:xs) = refillArrayWithTemp x : refill2DimArrayWithTemp xs where
+                        refillArrayWithTemp [] = []
+                        refillArrayWithTemp (y:ys)
+                            | y<30 = 0 : refillArrayWithTemp ys
+                            | y<40 = 1 : refillArrayWithTemp ys
+                            | otherwise = 2 : refillArrayWithTemp ys
                 putStrLn $ "时间戳: " ++ show ts
                 putStrLn $ "最高温度: " ++ show maxTemp ++ "°C"
-                putStrLn $ "所有温度: " ++ show temps
+                putStrLn $ "所有温度: " ++ show tempArray
+                putStrLn $ "警告温度: 已省略" -- ++ warnTempArray
                 putStrLn "--------------------------"
