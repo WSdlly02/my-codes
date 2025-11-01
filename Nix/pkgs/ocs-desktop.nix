243b9fb5155da8399291e47d85b4627e756b7483
wsdlly02 <wsdlly02@hotmail.com>
2025-11-02 01:44:37 +0800

    update ocs-desktop 2.8.5->2.8.21

--- Nix/pkgs/ocs-desktop.nix
+++ Nix/pkgs/ocs-desktop.nix
@@ -1,30 +1,37 @@
 {
   appimageTools,
   fetchurl,
+  libarchive,
   stdenv,
 }:
 let
-  version = "2.8.5";
+  version = "2.8.21";
   pname = "ocs-desktop";
   src =
     {
       x86_64-linux = fetchurl {
         url = "https://github.com/ocsjs/${pname}/releases/download/${version}/ocs-${version}-setup-linux-x86_64.AppImage";
-        hash = "sha256-SujCzoZMZ3YRJCEdrkDmFrYQtvyGAMDW/ragMI/Q+N0=";
+        hash = "sha256-2u+8Tmcriz+jLXAWXcqJyc/AV0MW574Kix2BajY0Gt0=";
       };
       aarch64-linux = fetchurl { };
     }
     .${stdenv.hostPlatform.system};
-  appimageContents = appimageTools.extract { inherit pname version src; };
+  appimageContents = appimageTools.extract {
+    inherit pname version src;
+    postExtract = ''
+      ${libarchive}/bin/bsdtar -xf $out/resources/bin/chrome/chrome.zip \
+        --strip-components=2 \
+        -C $out/resources/bin/chrome/
+      rm $out/resources/bin/chrome/chrome.zip
+    '';
+  };
 in
-appimageTools.wrapType2 rec {
-  inherit pname version src;
-
+appimageTools.wrapAppImage {
+  inherit pname version;
+  src = appimageContents;
   extraInstallCommands = ''
     install -m 444 -D ${appimageContents}/ocs\ desktop.desktop -t $out/share/applications
     substituteInPlace $out/share/applications/ocs\ desktop.desktop \
       --replace 'Exec=AppRun --no-sandbox %U' 'Exec=${pname} --no-sandbox %U'
-    cp -r ${appimageContents}/usr/lib $out
-    cp -r ${appimageContents}/usr/share $out
   '';
 }
