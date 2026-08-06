1140a2518f9397badf198b50d744ec3446447448
wsdlly02 <wsdlly02@hotmail.com>
2026-08-06 11:22:36 +0800

    chore: move smu-2024-ml/ to hackathons/

--- Nix/pkgs/mihomo-updater.nix
+++ Nix/pkgs/mihomo-updater.nix
@@ -3,7 +3,7 @@
 }:
 buildRustPackage {
   pname = "mihomo-updater";
-  version = "1.1.1";
+  version = "2.0.0";
   src = ../../SOPs/mihomo-updater;
   cargoLock.lockFile = ../../SOPs/mihomo-updater/Cargo.lock;
   cargoBuildFlags = [
