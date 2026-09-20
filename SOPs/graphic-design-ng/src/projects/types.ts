f71057c24ac91db7cfeb597569c9ab79510d3688
wsdlly02 <wsdlly02@hotmail.com>
2026-09-20 16:24:42 +0800

    Refactor: Remove frontend framework code and related files

--- SOPs/graphic-design-ng/src/projects/types.ts
+++
@@ -1,9 +0,0 @@
-import type { PosterScene } from "../types/scene";
-
-export interface PosterProject {
-  id: string;
-  name: string;
-  description: string;
-  scene: PosterScene;
-  exportFileName?: string;
-}
--- SOPs/slide-show-ng/frontend/src/DeckApp.tsx
+++
@@ -1,5 +0,0 @@
-import DemoDeck from './decks/demo/DemoDeck';
-
-export default function DeckApp() {
-  return <DemoDeck />;
-}
